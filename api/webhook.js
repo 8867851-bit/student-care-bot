ef3563561a76266bf459b8a0cace259d4bcad8f8
8867851-bit <8867851@student.triamudom.ac.th>
2026-03-19 07:53:36 +0700

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -1,34 +1,3 @@
-import fetch from 'node-fetch';
-
-export default async function handler(req, res) {
-  if (req.method !== 'POST') {
-    return res.status(200).send('OK');
-  }
-
-  try {
-    const body = req.body;
-
-    if (!body.events || body.events.length === 0) {
-      return res.status(200).send('OK');
-    }
-
-    const event = body.events[0];
-
-    await fetch('https://api.line.me/v2/bot/message/reply', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer4lUD0827K5XXnRyI9tJn9nXncbC9DHUuCSHMSOu01/UYrDLxnQGUKMGSWqoFoc+obKHhaxMC/GOTnHAJsMuT0s6M28wzzSyaziQG5cPinEsaEgehAEIT0BfXujeuCcQ7maJoTCh/VH11mA3l7NbUbQdB04t89/1O/w1cDnyilFU='
-      },
-      body: JSON.stringify({
-        replyToken: event.replyToken,
-        messages: [{ type: 'text', text: 'OK แล้ว 💛' }]
-      })
-    });
-
-    return res.status(200).send('OK');
-
-  } catch (err) {
-    return res.status(200).send('OK');
-  }
+export default function handler(req, res) {
+  return res.status(200).send('OK');
 }
